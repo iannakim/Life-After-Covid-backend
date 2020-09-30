fc2e8b10f352db992028923d57f106703daea3b3
iannakim <dear.hyunji@gmail.com>
2020-09-30 16:14:33 -0400

    routes set up

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,11 +13,23 @@ class CartsController < ApplicationController
 
     def create
         @cart = Cart.create(cart_params)
+        render json: @cart
     end 
+
+
+    def findUserCart
+        @found_cart = Cart.find_by(user_id: params[:user_id])
+        if @found_cart
+            render json: @found_cart
+        else
+            render json: {error: "The user has no cart"}
+        end
+    end
+
 
     private 
 
-    def cart_parmas
+    def cart_params
         params.permit(:user_id)
     end
 
